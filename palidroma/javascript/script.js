63e692e2190c43fa04cb628bf210e5d102f6e574
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-14 16:22:13 +0100

    added full version

--- palidroma/javascript/script.js
+++ palidroma/javascript/script.js
@@ -7,6 +7,28 @@ const userString = prompt("Inserisci una parola", "Onorarono").trim().toLowerCas
 const wordPlaidroma = palidroma(userString);
 
 function palidroma(wordToCheck) {
+    let result = "";
+    for (let i = wordToCheck.length - 1; i >= 0; i--) {
+        result += wordToCheck[i];
+    }
+    let palOrNot;
+    if (userString === result) {
+        palOrNot = "Palidroma";
+    } else {
+        palOrNot = "No Palidroma";
+    }
+    return palOrNot;
+
+}
+
+console.log(wordPlaidroma);
+
+/* VERSIONE RISTRETTA
+const userString = prompt("Inserisci una parola", "Onorarono").trim().toLowerCase();
+
+const wordPlaidroma = palidroma(userString);
+
+function palidroma(wordToCheck) {
     let result = wordToCheck.split("").reverse().join("");
     let palOrNot;
     if (userString === result) {
@@ -18,3 +40,4 @@ function palidroma(wordToCheck) {
 }
 
 console.log(wordPlaidroma);
+*/
