52d52544dc12bf59ee761cc4987eb810d5cee943
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-14 13:34:21 +0100

    added topic

--- palidroma/javascript/script.js
+++ palidroma/javascript/script.js
@@ -1,2 +1,3 @@
-let prova = "Everything is Working";
-console.log(prova)
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
