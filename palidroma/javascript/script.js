468f1d02dd4742c5d9ecb07461c09aab558483ce
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-14 20:01:59 +0100

    aggiunti commenti

--- palidroma/javascript/script.js
+++ palidroma/javascript/script.js
@@ -2,16 +2,21 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+// chiedo all'utente di scrivere la parola da controllare
 const userString = prompt("Inserisci una parola", "Onorarono").trim().toLowerCase();
 
+// richiamo la funzione palindroma
 const wordPlaidroma = palidroma(userString);
 
+// funzione per invertire la stringa e poi verificare se è palindroma
 function palidroma(wordToCheck) {
     let result = "";
+    // imposto il ciclo for in modo che parta dall'ultima lettera e proceda all'indietro
     for (let i = wordToCheck.length - 1; i >= 0; i--) {
         result += wordToCheck[i];
     }
     let palOrNot;
+    // verifico se le due stringhe sono polindrome
     if (userString === result) {
         palOrNot = "Palidroma";
     } else {
@@ -20,7 +25,7 @@ function palidroma(wordToCheck) {
     return palOrNot;
 
 }
-
+// stampo nella console il risultato
 console.log(wordPlaidroma);
 
 /* VERSIONE RISTRETTA
