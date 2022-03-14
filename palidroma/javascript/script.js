d67ea833490f1c1777513c308dc7ff02f796f7d0
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-14 14:31:20 +0100

    completed palidrome words exercise

--- palidroma/javascript/script.js
+++ palidroma/javascript/script.js
@@ -1,3 +1,20 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+const userString = prompt("Inserisci una parola", "Onorarono").trim().toLowerCase();
+
+const wordPlaidroma = palidroma(userString);
+
+function palidroma(wordToCheck) {
+    let result = wordToCheck.split("").reverse().join("");
+    let palOrNot;
+    if (userString === result) {
+        palOrNot = "Palidroma";
+    } else {
+        palOrNot = "No Palidroma";
+    }
+    return palOrNot;
+}
+
+console.log(wordPlaidroma);
